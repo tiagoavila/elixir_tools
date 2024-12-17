939e1b7b724ea54232648a85a91a2a09168e8100
Tiago Ávila <tiago.avila09@gmail.com>
2024-12-17 10:37:32 -0300

    added missing file

--- assets/js/textarea.js
+++ assets/js/textarea.js
@@ -1,6 +1,7 @@
 export const TextAreaHook = {
     mounted() {
         document.getElementById("input-area").focus();
+
         this.el.addEventListener("input", e => {
             this.pushEvent("parse_text", { text: e.target.value })
         })
